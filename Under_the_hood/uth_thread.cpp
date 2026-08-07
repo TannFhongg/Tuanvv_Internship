ba0ee944ea9a2a0835f8f21939d052c04d4931f8
tuanvv <tuannamtrachqb@gmail.com>
2026-08-07 17:10:44 +0700

    under the hood thread

--- Under_the_hood/uth_thread.cpp
+++ Under_the_hood/uth_thread.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
+
 class thread
 {
 public:
@@ -31,7 +36,43 @@ public:
     void join();
     void detach();
     id get_id() const noexcept;
-    native_handle_type native_handle();
+    // native_handle_type native_handle();
 
     static unsigned int hardware_concurrency() noexcept;
 };
+
+struct LoopMessage
+{
+    const std::string message;
+    const int delay;
+
+    LoopMessage(std::string message, int delay) : message(message), delay(delay) {}
+
+    // Function-call operator called by the thread class.
+    void operator()()
+    {
+        while (true)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(delay));
+            std::cout << " [INFO] thread id = " << std::this_thread::get_id()
+                      << " ; " << message << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    thread thread_messageA{LoopMessage{"Hello world", 10}};
+    thread thread_messageB{LoopMessage{"Hello world", 10}};
+
+    if (thread_messageA.joinable())
+    {
+        thread_messageA.join();
+    }
+    if (thread_messageB.joinable())
+    {
+        thread_messageB.join();
+    }
+    
+    return 0;
+}
