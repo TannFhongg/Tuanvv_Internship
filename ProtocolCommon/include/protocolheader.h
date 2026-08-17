4ed6ba079e821c7df079e8d3d360f85d66153c9a
tuanvv <tuannamtrachqb@gmail.com>
2026-08-17 14:54:55 +0700

    3.2.A thiet ke dulieu header

---
+++ ProtocolCommon/include/protocolheader.h
@@ -0,0 +1,17 @@
+
+#pragma once
+#include <QtGlobal> 
+#include "protocoltypes.h"
+
+namespace MiniCloud::Protocol {
+    
+    struct ProtocolHeader
+    {
+        quint32 magic = 0;
+        quint16 protocolVersion = 0;
+        MessageType messageType = MessageType::Invalid;
+        quint32 payloadLength = 0;
+        RequestId requestId = 0;
+        TaskId taskId = 0;
+    };
+} 
---
+++ ProtocolCommon/include/protocoltypes.h
@@ -0,0 +1,27 @@
+// ID và các enum protocol.
+/*
+
+*/
+#pragma once
+#include <QtGlobal> // để dùng quint64
+namespace MiniCloud::Protocol
+{
+    using RequestId = quint64;
+    using TaskId = quint64;
+
+     enum class MessageType : quint16 {
+        Invalid = 0,
+        AuthenticateRequest = 1,
+        AuthenticateResponse = 2,
+        ErrorResponse = 3,
+        FileChunk = 4
+    };
+
+     enum class ErrorCode : quint16 {
+        None = 0,
+        InvalidRequest = 1,
+        AuthenticationFailed = 2,
+        FileNotFound = 3,
+        InternalServerError = 4
+    };
+}
