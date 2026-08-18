a4e1e94007beae00c0bfb57e72a06be0a0e8ada6
tuanvv <tuannamtrachqb@gmail.com>
2026-08-18 16:00:09 +0700

    refactor test function

--- ProtocolCommon/tests/protocolcodectest.cpp
+++ ProtocolCommon/tests/protocolcodectest.cpp
@@ -7,23 +7,34 @@ class ProtocolCodecTest : public QObject
     Q_OBJECT
 
 private slots:
-    void encode_and_bigEndian()
+    void encode()
+    {
+        MiniCloud::Protocol::ProtocolHeader header;
+        header.protocolMagic = MiniCloud::Protocol::protocolMagic;
+        header.protocolVersion = MiniCloud::Protocol::protocolVersion;
+        header.messageType = MiniCloud::Protocol::MessageType::AuthenticateRequest;
+        header.payloadLength = 0;
+        header.requestId = 0;
+        header.taskId = 0;
+
+        const QByteArray encoded = MiniCloud::Protocol::serializeHeader(header);
+        QCOMPARE(encoded.size(), static_cast<qsizetype>(MiniCloud::Protocol::protocolWireHeaderSize));
+        qDebug() << "Encoded header size:" << encoded.size();
+    }
+
+    void testBigEndian()
     {
         MiniCloud::Protocol::ProtocolHeader header;
         header.protocolMagic = MiniCloud::Protocol::protocolMagic;
         header.protocolVersion = MiniCloud::Protocol::protocolVersion;
         header.messageType = MiniCloud::Protocol::MessageType::AuthenticateRequest;
         header.payloadLength = 5;
-        header.requestId = 0x0102030405060708;
+        header.requestId = 0x0102030405060708 ;
         header.taskId = 0;
-
         const QByteArray encoded = MiniCloud::Protocol::serializeHeader(header);
-        QCOMPARE(encoded.size(), static_cast<qsizetype>(MiniCloud::Protocol::protocolWireHeaderSize));
-        qDebug() << "Encoded header size:" << encoded.size();
-        const QByteArray expect = QByteArray::fromHex("4D434C44000100010000000501020304050607080000000000000000");
-        QCOMPARE(encoded,expect);
-        qDebug() <<"Before:" << encoded.toHex(' ');
-        qDebug() <<"After:" << expect.toHex(' ') ;
+        const QByteArray expected = QByteArray::fromHex("4D434C44000100010000000501020304050607080000000000000000");
+        QCOMPARE(encoded,expected);
+
     }
 
 };
