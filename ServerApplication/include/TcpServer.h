3aff8f9ba6f37edada4008e877c76e6d1b824d23
tuanvv <tuannamtrachqb@gmail.com>
2026-08-21 15:36:49 +0700

    khoi tao listen ip/port

---
+++ ServerApplication/include/TcpServer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <QTcpServer>
+#include <QObject>
+#include <QHostAddress>
+
+class ClientSession;
+
+class TcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TcpServer(QObject *parent = nullptr);
+    bool startListening(const QHostAddress &address, quint16 port);
+    void stop();
+    bool isListening() const;
+    QString errorString() const;
+    quint16 serverPort() const;
+
+signals:
+    void clientConnected();
+    void clientDisconnected();
+    void clientRejected();
+    void listenFailed(const QString &errorString);
+
+private slots:
+    void onNewConnection();
+    void onSessionFinished(ClientSession *session);
+
+private:
+    QTcpServer *m_server;
+    ClientSession *m_activeSession = nullptr;
+};
---
+++ ServerApplication/src/TcpServer.cpp
@@ -0,0 +1,95 @@
+#include "TcpServer.h"
+#include "ClientSession.h"
+#include <QtGlobal>
+
+TcpServer::TcpServer(QObject *parent)
+    : QObject(parent), m_server(new QTcpServer(this)), m_activeSession(nullptr)
+{
+    connect(m_server,                     // doi tuong phat signal
+            &QTcpServer::newConnection,   // signal phat ra khi co ket noi moi
+            this,                         // doi tuong nhan signal , TcpServer hien tai
+            &TcpServer::onNewConnection); // slot duoc goi khi signal duoc phat ra
+}
+
+bool TcpServer::startListening(const QHostAddress &address, quint16 port)
+{
+    if (m_server->isListening())
+    {
+        emit listenFailed("Server is already listening.");
+        return false;
+    }
+    else if (!m_server->listen(address, port))
+    {
+        emit listenFailed(m_server->errorString());
+        return false;
+    }
+    return true;
+}
+
+void TcpServer::stop()
+{
+    if (m_server->isListening())
+    {
+        m_server->close();
+    }
+
+    if (m_activeSession)
+    {
+        m_activeSession->closeSession();
+    }
+}
+
+bool TcpServer::isListening() const
+{
+    return m_server->isListening();
+}
+
+QString TcpServer::errorString() const
+{
+    return m_server->errorString();
+}
+
+quint16 TcpServer::serverPort() const
+{
+    return m_server->serverPort();
+}
+
+void TcpServer::onNewConnection()
+{
+    QTcpSocket *socket = m_server->nextPendingConnection();
+    if (socket == nullptr)
+        return;
+
+    if(m_server -> isListening() && m_activeSession == nullptr)
+    {
+        m_activeSession = new ClientSession(socket, this);
+        connect(m_activeSession, &ClientSession::sessionFinished, this, &TcpServer::onSessionFinished);
+        emit clientConnected();
+    }
+    
+    if (m_activeSession == nullptr)
+    {
+        m_activeSession = new ClientSession(socket, this);
+        connect(m_activeSession, &ClientSession::sessionFinished, this, &TcpServer::onSessionFinished);
+        emit clientConnected();
+        return;
+    }
+    socket->disconnectFromHost();
+    socket->deleteLater();
+    emit clientRejected();
+}
+
+void TcpServer::onSessionFinished(ClientSession *session)
+{
+    if (session == nullptr)
+        return;
+
+    if (session != m_activeSession)
+    {
+        session->deleteLater();
+        return;
+    }
+    m_activeSession = nullptr;
+    session->deleteLater();
+    emit clientDisconnected();
+}
