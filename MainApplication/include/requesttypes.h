87543ee9a86690cd1d22041ac9d4d7fdb438c07c
tuanvv <tuannamtrachqb@gmail.com>
2026-08-25 15:18:53 +0700

    type request of client

---
+++ MainApplication/include/requesttypes.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <QMetaType>
+#include <protocoltypes.h>
+namespace MiniCloud::Client
+{
+    enum class RequestDestination
+    {
+        Invalid,
+        License,
+        File,
+        Task
+    };
+
+    enum class RequestSendStatus
+    {
+        Accepted,
+        Failed
+    };
+
+    enum class RequestDispatchError
+    {
+        None,
+        Unknown,
+        InvalidRequest,
+        InvalidDestination,
+        NotConnected,
+        InvalidMessageType,
+        PayloadTooLarge,
+        FrameEncodingFailed,
+        SocketWriteFailed,
+        ConnectionLost,
+        RequestTimeout,
+        ResponseTypeMismatch,
+        UnexpectedResponse
+    };
+
+    struct RequestSendResult
+    {
+        RequestSendStatus status = RequestSendStatus::Failed;
+        MiniCloud::Protocol::RequestId requestId = 0;
+        RequestDispatchError errorCode = RequestDispatchError::Unknown;
+    };
+}
+
+Q_DECLARE_METATYPE(MiniCloud::Client::RequestDestination)
+Q_DECLARE_METATYPE(MiniCloud::Client::RequestDispatchError)
