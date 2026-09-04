c063d1bf4564b795f715d00f77125b036eade24a
tuanvv <tuannamtrachqb@gmail.com>
2026-09-04 16:07:32 +0700

    them gioi han cho file chunk khi uploah,download

--- ProtocolCommon/include/protocolconstants.h
+++ ProtocolCommon/include/protocolconstants.h
@@ -8,6 +8,7 @@ inline constexpr quint32 protocolMagic = 0x4D434C44; // MCLD
 inline constexpr quint16 protocolVersion = 1;
 inline constexpr quint32 protocolWireHeaderSize = 28; // bytes
 inline constexpr quint32 protocolMaxControlPayloadSize = 1024u * 1024u;
-inline constexpr quint32 protocolDefaultFileChunkSize = 64u * 1024u;
+inline constexpr quint32 protocolMaxFileChunkDataBytes = 64u * 1024u; // 65536 bytes
+inline constexpr quint32 protocolDefaultFileChunkSize = protocolMaxFileChunkDataBytes;
 inline constexpr quint32 protocolMaxFramePayloadSize = 1024u * 1024u;
 }
